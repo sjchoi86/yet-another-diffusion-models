755aee14d13be4c59df77e79a713121f272302b9
seungwoos <tmddngood@gmail.com>
2023-03-07 15:20:29 +0900

    fixed to make all tensors in same device

--- code/ddpm.py
+++ code/ddpm.py
@@ -3,7 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
-from util import np2torch,torch2np,gather_and_reshape,kernel_se,get_rkhs_proj
+from util import np2torch,torch2np,gather_and_reshape,kernel_se,get_rkhs_proj,power_test
 from model import DenoisingDenseUNetClass
 
 # DDPM constants
@@ -71,19 +71,19 @@ def forward_hilbert_diffusion_sample(x_0,K_chols,steps,dc,noise_rate=1.0,RKHS_pr
         RKHS_projs: torch.Tensor [D x L x L]
     """
     sqrt_alphas_bar_t = gather_and_reshape(
-        values=dc['sqrt_alphas_bar'],steps=steps,x_shape=x_0.shape) # [B x 1 x 1]
+        values=dc['sqrt_alphas_bar'],steps=steps,x_shape=x_0.shape).to(device) # [B x 1 x 1]
     sqrt_one_minus_alphas_bar_t = gather_and_reshape(
-        values=dc['sqrt_one_minus_alphas_bar'],steps=steps,x_shape=x_0.shape) # [B x 1 x 1]
-    x_t_mean = sqrt_alphas_bar_t * x_0 # [B x D x L]
+        values=dc['sqrt_one_minus_alphas_bar'],steps=steps,x_shape=x_0.shape).to(device) # [B x 1 x 1]
+    x_t_mean = sqrt_alphas_bar_t * x_0 # [B x D x L] 
     x_t_std = sqrt_one_minus_alphas_bar_t # [B x D x L]
     # Correlated noise sampling
     if noise_type == 'Gaussian':
-        noise = torch.randn_like(input=x_0) # [B x D x L]
+        noise = torch.randn_like(input=x_0, device=device) # [B x D x L]
     elif noise_type == 'Uniform':
-        noise = 6.0*torch.rand_like(input=x_0)-3.0 # [B x D x L]
+        noise = 6.0*torch.rand_like(input=x_0, device=device)-3.0 # [B x D x L]
     else:
         print ("[forward_hilbert_diffusion_sample] Unknown [%s]"%(noise_type))
-        noise = torch.randn_like(input=x_0) # [B x D x L]
+        noise = torch.randn_like(input=x_0, device=device) # [B x D x L]
     noise_expand = noise[:,:,:,None] # [B x D x L x 1]
     K_chols_torch_tile = torch.tile(input=K_chols,dims=(x_0.shape[0],1,1,1)) # [B x D x L x L]
     correlated_noise_permuted = K_chols_torch_tile @ noise_expand # [B x D x L x 1]
@@ -106,7 +106,7 @@ def forward_hilbert_diffusion_sample(x_0,K_chols,steps,dc,noise_rate=1.0,RKHS_pr
 def get_ddpm_loss(model,x_batch,K_chols,A=None,V=None,
                   t=0,dc={},noise_rate=1.0,RKHS_projs=None,noise_type='Gaussian',
                   l1_w=0.0,l2_w=1.0,huber_w=0.0,smt_l1_w=0.0,
-                  vel_w=1e-6,acc_w=1e-6):
+                  vel_w=1e-6,acc_w=1e-6,device='cpu'):
     """
         x_batch: [B x D x L]
         K_chols: [D x L x L]
@@ -116,19 +116,21 @@ def get_ddpm_loss(model,x_batch,K_chols,A=None,V=None,
     # Sample from forward diffusion
     x_noisy,noise = forward_hilbert_diffusion_sample(
         x_0=x_batch,K_chols=K_chols,steps=t,dc=dc,noise_rate=noise_rate,
-        RKHS_projs=RKHS_projs,noise_type=noise_type
+        RKHS_projs=RKHS_projs,noise_type=noise_type, device=device
     ) # [B x D x L]
 
     # Predict noise
     x_noisy_flat = x_noisy.reshape(x_noisy.shape[0],-1) # [B x DL]
     noise_pred = model(x_noisy_flat, t) # [B x DL]
     noise_pred_unflat = noise_pred.reshape_as(x_batch) # [B x D x L]
-    # x_t_minus_one = x_batch - 0.1 * noise_pred_unflat # [B x D x L]
-    x_t_minus_one = x_batch - np.sqrt(2e-1) * noise_pred_unflat
+    # TODO: Need to change dt for different timesteps
+    # x_t_minus_one = x_batch - 0.1 * noise_pred_unflat # [B x D x L] linspace(0, 1, 100) -> np.sqrt(0.01) = 0.1
+    # x_t_minus_one = x_batch - np.sqrt(2e-1) * noise_pred_unflat # linspace(-10., 10., 100)
+    x_t_minus_one = x_batch - np.sqrt(0.979) * noise_pred_unflat # linspace(0., 23., 24) -> np.sqrt(23/24) ~= np.sqrt(0.979)
 
     # Smoothing loss
     if V is not None:
-        V_expand = V[None,None,:,:] # [1 x 1 x L x L]
+        V_expand = V[None,None,:,:].to(device) # [1 x 1 x L x L]
         V_expand_tile = torch.tile(input=V_expand,dims=(x_t_minus_one.shape[0],x_t_minus_one.shape[1],1,1)) # [B x D x L x L]
         x_t_minus_one_expand = x_t_minus_one[:,:,:,None] # [B x D x L x 1]
         vel = V_expand_tile @ x_t_minus_one_expand # [B x D x L x 1]
@@ -138,7 +140,7 @@ def get_ddpm_loss(model,x_batch,K_chols,A=None,V=None,
     else:
         vel_loss = 0.0
     if A is not None:
-        A_expand = A[None,None,:,:] # [1 x 1 x L x L]
+        A_expand = A[None,None,:,:].to(device) # [1 x 1 x L x L]
         A_expand_tile = torch.tile(input=A_expand,dims=(x_t_minus_one.shape[0],x_t_minus_one.shape[1],1,1)) # [B x D x L x L]
         x_t_minus_one_expand = x_t_minus_one[:,:,:,None] # [B x D x L x 1]
         acc = A_expand_tile @ x_t_minus_one_expand # [B x D x L x 1]
@@ -169,7 +171,7 @@ def eval_hddpm_1D(
     B=3,M=8,device='cpu',
     RKHS_PROJECTION_EACH_X_T=False,
     PLOT_GENERATED_TRAJECTORIES_AT_ONCE=True,
-    PLOT_ANCESTRAL_STEPS=True):
+    PLOT_ANCESTRAL_STEPS=True,return_output=False):
     """
         Evaluate Hilbert-space DDPM for 1D
     """
@@ -181,7 +183,7 @@ def eval_hddpm_1D(
     steps = torch.zeros(B).type(torch.long).to(device) # [B]
     _,correlated_noise = forward_hilbert_diffusion_sample(
         x_0=x_0_dummy,K_chols=K_chols,steps=steps,dc=dc,noise_rate=1.0,
-        RKHS_projs=RKHS_projs,noise_type='Gaussian')
+        RKHS_projs=RKHS_projs,noise_type='Gaussian',device=device)
     x_T = correlated_noise.clone() # [B x D x L]
     x_t = x_T.clone()
     # Generate
@@ -215,7 +217,7 @@ def eval_hddpm_1D(
         steps = torch.zeros(B).type(torch.long) # [B]
         _,noise_t = forward_hilbert_diffusion_sample(
             x_0=x_0_dummy,K_chols=K_chols,steps=steps,dc=dc,noise_rate=1.0,
-            RKHS_projs=RKHS_projs,noise_type='Gaussian'
+            RKHS_projs=RKHS_projs,noise_type='Gaussian',device=device
         )
         noise_t # [B x D x L]
         # Compute posterior variance
@@ -245,7 +247,7 @@ def eval_hddpm_1D(
             plt.grid('on')
             plt.title('Training Data (D:[%d])'%(d_idx),fontsize=8); 
             # plt.xlim(0,+1); plt.ylim(-2.5,+2.5)
-            plt.xlim(-10,10); plt.ylim(-100, 100)
+            # plt.xlim(-10,10); plt.ylim(-100, 100)
             plt.subplot(1,2,2)
             for b_idx in range(B):
                 # Plot sampled trajectoires
@@ -277,7 +279,7 @@ def eval_hddpm_1D(
                 plt.grid('on')
                 plt.title('batch:[%d/%d] dim:[%d] Data'%(b_idx,B,d_idx),fontsize=8); 
                 # plt.xlim(0,+1); plt.ylim(-2.5,+2.5)
-                plt.xlim(-10,10); plt.ylim(-100, 100)
+                # plt.xlim(-10,10); plt.ylim(-100, 100)
 
                 plt.subplot(1,2,2)
                 if len(x_0.shape) == 2:
@@ -289,7 +291,7 @@ def eval_hddpm_1D(
                 plt.plot(times[:,0],torch2np(x_t)[b_idx,d_idx,:],ls='-',color='k'); plt.grid('on')
                 plt.title('dim:[%d]'%(d_idx),fontsize=8); 
                 # plt.xlim(0,+1); plt.ylim(-2.5,+2.5)
-                plt.xlim(-10,10); plt.ylim(-100, 100)
+                # plt.xlim(-10,10); plt.ylim(-100, 100)
 
                 plt.show()
     
@@ -309,13 +311,16 @@ def eval_hddpm_1D(
                             plt.plot(times[:,0],torch2np(x_0)[m_idx,d_idx,:],ls='-',color='b',lw=1/2); 
                     plt.plot(times[:,0],torch2np(x_ts[t][b_idx,d_idx,:]),ls='-',color='k',lw=1) # generated
                     # plt.xlim(0,+1); plt.ylim(-2.5,+2.5); plt.grid('on')
-                    plt.xlim(-10,10); plt.ylim(-100, 100)
+                    # plt.xlim(-10,10); plt.ylim(-100, 100)
 
                     plt.title('dim:[%d] t:[%d]'%(d_idx,t),fontsize=8)
                 plt.show()    
 
-    # Back to train mode
-    model.train()
+    if return_output:
+        return x_t
+    else:
+        # Back to train mode
+        model.train()
 
 def ddpm_train_wrapper(
     times,x_0,dc,
@@ -332,7 +337,6 @@ def ddpm_train_wrapper(
     ):
     train_losses, valid_losses = [], []
     x_0_train, x_0_valid = x_0[:BATCH_SIZE], x_0[BATCH_SIZE:]
-    print(x_0_train.shape, x_0_valid.shape, x_0.shape)
     # Some infos (x_0 is either [D x L] or [M x D x L] )
     L = times.shape[0]
     if len(x_0_train.shape) == 2:
@@ -340,7 +344,6 @@ def ddpm_train_wrapper(
     elif len(x_0_train.shape) == 3:
         M = x_0_train.shape[0]
         D = x_0_train.shape[1]
-
     # Set Hilbert forward process
     hyp_lens = [hyp_len_forward]*D # length parameter per each dimension
     K_chols_np = np.zeros(shape=(D,L,L)) # [D x L x L]
@@ -377,30 +380,30 @@ def ddpm_train_wrapper(
         optm.zero_grad()
         t = torch.randint(0, dc['T'],(BATCH_SIZE,),device=device).long() # [B]
         if len(x_0_train.shape) == 2:
-            x_batch = torch.tile(x_0_train[None,:,:],dims=(BATCH_SIZE,1,1)) # [B x D x L]
+            x_batch = torch.tile(x_0_train[None,:,:],dims=(BATCH_SIZE,1,1)).to(device) # [B x D x L]
             loss,info = get_ddpm_loss(
                 model=model,x_batch=x_batch,K_chols=K_chols,A=A,V=V,
                 t=t,dc=dc,noise_rate=1.0,
                 RKHS_projs=RKHS_projs,noise_type='Gaussian',
-                l1_w=l1_w,l2_w=l2_w,huber_w=huber_w,smt_l1_w=smt_l1_w,vel_w=vel_w,acc_w=acc_w)
+                l1_w=l1_w,l2_w=l2_w,huber_w=huber_w,smt_l1_w=smt_l1_w,vel_w=vel_w,acc_w=acc_w,device=device)
         else:
             idx = np.random.choice(x_0_train.shape[0],BATCH_SIZE)
-            x_batch = x_0_train[idx,:,:] # [B x D x L]
+            x_batch = x_0_train[idx,:,:].to(device) # [B x D x L]
             loss,loss_info = get_ddpm_loss(
                 model=model,x_batch=x_batch,K_chols=K_chols,A=A,V=V,
                 t=t,dc=dc,noise_rate=1.0,
                 RKHS_projs=RKHS_projs,noise_type='Gaussian',
-                l1_w=l1_w,l2_w=l2_w,huber_w=huber_w,smt_l1_w=smt_l1_w,vel_w=vel_w,acc_w=acc_w)
+                l1_w=l1_w,l2_w=l2_w,huber_w=huber_w,smt_l1_w=smt_l1_w,vel_w=vel_w,acc_w=acc_w,device=device)
         loss.backward(); optm.step(); lr_schd.step()
 
         model.eval()
         with torch.no_grad():
-            x_batch = x_0_valid
+            x_batch = x_0_valid.to(device)
             val_loss, val_loss_info = get_ddpm_loss(
                 model=model,x_batch=x_batch,K_chols=K_chols,A=A,V=V,
                 t=t,dc=dc,noise_rate=1.0,
                 RKHS_projs=RKHS_projs,noise_type='Gaussian',
-                l1_w=l1_w,l2_w=l2_w,huber_w=huber_w,smt_l1_w=smt_l1_w,vel_w=vel_w,acc_w=acc_w
+                l1_w=l1_w,l2_w=l2_w,huber_w=huber_w,smt_l1_w=smt_l1_w,vel_w=vel_w,acc_w=acc_w,device=device
             )
 
         # Printout
@@ -418,12 +421,160 @@ def ddpm_train_wrapper(
         # Plot evaluation results
         if ((it % (MAX_ITER//N_PLOT)) == 0) or (it==(MAX_ITER-1)):
             eval_hddpm_1D(
-                model,dc,K_chols,RKHS_projs,times,x_0_train,
+                model,dc,K_chols,RKHS_projs,times,x_0_valid,
                 B=n_sample,M=8,device=device,
                 RKHS_PROJECTION_EACH_X_T=RKHS_PROJECTION_EACH_X_T,
-                PLOT_ANCESTRAL_STEPS=False)    
+                PLOT_ANCESTRAL_STEPS=False,return_output=False)    
         
         train_losses.append(loss.item())
         valid_losses.append(val_loss.item())
-    
-    return train_losses, valid_losses
+
+    # Generation
+
+    outs = eval_hddpm_1D(
+        model,dc,K_chols,RKHS_projs,times,x_0_valid,
+        B=n_sample,M=8,device=device,
+        RKHS_PROJECTION_EACH_X_T=RKHS_PROJECTION_EACH_X_T,
+        PLOT_ANCESTRAL_STEPS=False,return_output=True)    
+ 
+    return train_losses, valid_losses, outs
+
+def ddpm_trainer(
+    times,
+    train_loader,
+    valid_loader,
+    test_loader,
+    dc,
+    actv=nn.ReLU(),
+    hyp_len_forward=0.1,
+    hyp_len_projection=0.1,
+    hyp_sig2w=1e-6,
+    n_sample=20,
+    base_lr=1e-3,MAX_ITER=1e5,BATCH_SIZE=128,
+    l1_w=1.0,l2_w=1.0,huber_w=0.0,smt_l1_w=0.0,
+    A=None,V=None,vel_w=0.0,acc_w=0.0,
+    RKHS_PROJECTION_EACH_X_T=False,
+    device='cpu'
+    ):
+    train_losses, valid_losses = [], []
+    y_0_train = next(iter(train_loader))[1]
+    # Some infos (x_0 is either [D x L] or [M x D x L] )
+    L = times.shape[0]
+    if len(y_0_train.shape) == 2:
+        D = y_0_train.shape[0]
+    elif len(y_0_train.shape) == 3:
+        M = y_0_train.shape[0]
+        D = y_0_train.shape[1]
+
+    # Set Hilbert forward process
+    hyp_lens = [hyp_len_forward]*D # length parameter per each dimension
+    K_chols_np = np.zeros(shape=(D,L,L)) # [D x L x L]
+    for d_idx in range(D):
+        hyp_len = hyp_lens[d_idx]
+        if hyp_len == 0:
+            K = np.eye(L,L) # [L x L]
+        else:
+            K = kernel_se(times,times,hyp={'gain':1.0,'len':hyp_len}) # [L x L]
+        K_chols_np[d_idx,:,:] = np.linalg.cholesky(K+hyp_sig2w*np.eye(L,L)) # [L x L]
+    K_chols = np2torch(K_chols_np,device=device) # [D x L x L]
+    # Set RHKS projections
+    hyp_lens = [hyp_len_projection]*D # length parameter per each dimension
+    RKHS_projs_np = np.zeros(shape=(D,L,L)) # [D x L x L]
+    for d_idx in range(D):
+        hyp_len = hyp_lens[d_idx]
+        if hyp_len == 0:
+            RKHS_projs_np[d_idx,:,:] = np.eye(L,L) # [L x L]
+        else:
+            RKHS_projs_np[d_idx,:,:] = get_rkhs_proj(times=times,hyp_len=hyp_len,meas_std=hyp_sig2w) # [L x L]
+    RKHS_projs = np2torch(RKHS_projs_np,device=device) # [D x L x L]
+    # Model
+    model = DenoisingDenseUNetClass(
+        D=D,L=L,pos_emb_dim=32,h_dims=[128,64,32],z_dim=32,
+        actv=actv,USE_POS_EMB=True,RKHS_projs=RKHS_projs)
+    model.to(device)
+    # Train
+    # model.train()
+    optm = torch.optim.Adam(
+        model.parameters(),lr=base_lr,betas=(0.9, 0.95),eps=1e-04,weight_decay=1e-08)
+    lr_schd = torch.optim.lr_scheduler.StepLR(optm,step_size=int(0.4*MAX_ITER),gamma=0.5)
+    for it in range(MAX_ITER):
+        train_loss, valid_loss = 0.0, 0.0
+        train_num, valid_num = 0, 0
+        
+        # Train
+        for _, y_train in train_loader:
+            y_train = y_train.to(device)
+            model.train()
+            optm.zero_grad()
+            t = torch.randint(0, dc['T'],(y_train.shape[0],),device=device).long() # [B]
+            if len(y_train.shape) == 2:
+                y_train = torch.tile(y_train[None,:,:],dims=(BATCH_SIZE,1,1)).to(device) # [B x D x L]
+                loss,info = get_ddpm_loss(
+                    model=model,x_batch=y_train,K_chols=K_chols,A=A,V=V,
+                    t=t,dc=dc,noise_rate=1.0,
+                    RKHS_projs=RKHS_projs,noise_type='Gaussian',
+                    l1_w=l1_w,l2_w=l2_w,huber_w=huber_w,smt_l1_w=smt_l1_w,vel_w=vel_w,acc_w=acc_w,device=device)
+            else:
+                # idx = np.random.choice(y_train.shape[0],BATCH_SIZE)
+                # x_batch = y_train[idx,:,:].to(device) # [B x D x L]
+                loss,loss_info = get_ddpm_loss(
+                    model=model,x_batch=y_train,K_chols=K_chols,A=A,V=V,
+                    t=t,dc=dc,noise_rate=1.0,
+                    RKHS_projs=RKHS_projs,noise_type='Gaussian',
+                    l1_w=l1_w,l2_w=l2_w,huber_w=huber_w,smt_l1_w=smt_l1_w,vel_w=vel_w,acc_w=acc_w,device=device)
+            loss.backward(); optm.step(); lr_schd.step()
+            train_loss += loss.item() * y_train.shape[0]
+            train_num += y_train.shape[0]
+
+        # Validation
+        model.eval()
+        with torch.no_grad():
+            for _, y_train in valid_loader:
+                t = torch.randint(0, dc['T'],(y_train.shape[0],),device=device).long() # [B]
+                y_train = y_train.to(device)
+                val_loss, val_loss_info = get_ddpm_loss(
+                    model=model,x_batch=y_train,K_chols=K_chols,A=A,V=V,
+                    t=t,dc=dc,noise_rate=1.0,
+                    RKHS_projs=RKHS_projs,noise_type='Gaussian',
+                    l1_w=l1_w,l2_w=l2_w,huber_w=huber_w,smt_l1_w=smt_l1_w,vel_w=vel_w,acc_w=acc_w,device=device
+                )
+                valid_loss += val_loss.item() * y_train.shape[0]
+                valid_num += y_train.shape[0]
+
+        # Printout
+        N_PRINT,N_PLOT = 20,10
+        if ((it % (MAX_ITER//N_PRINT)) == 0) or (it==(MAX_ITER-1)):
+            print ("[%d/%d][%.2f%%] train loss:[%.3f]"%(it,MAX_ITER,100*it/MAX_ITER,train_loss / train_num))
+            for key in loss_info.keys():
+                print ("  [%s]:[%.3f]"%(key,loss_info[key].item()))
+        
+        if ((it % (MAX_ITER//N_PRINT)) == 0) or (it==(MAX_ITER-1)):
+            print ("[%d/%d][%.2f%%] valid loss:[%.3f]"%(it,MAX_ITER,100*it/MAX_ITER,valid_loss / valid_num))
+            for key in val_loss_info.keys():
+                print ("  [%s]:[%.3f]"%(key,val_loss_info[key].item()))
+
+        # Plot evaluation results
+        if ((it % (MAX_ITER//N_PLOT)) == 0) or (it==(MAX_ITER-1)):
+            with torch.no_grad():
+                _, y_valid = next(iter(valid_loader))
+                y_valid = y_valid.to(device)
+                eval_hddpm_1D(
+                    model,dc,K_chols,RKHS_projs,times,y_valid,
+                    B=n_sample,M=8,device=device,
+                    RKHS_PROJECTION_EACH_X_T=RKHS_PROJECTION_EACH_X_T,
+                    PLOT_ANCESTRAL_STEPS=False,return_output=False)    
+        
+        train_losses.append(loss.item())
+        valid_losses.append(val_loss.item())
+
+    # Generation
+
+    for x_test, y_test in test_loader:
+        y_test = y_test.to(device)
+        outs = eval_hddpm_1D(
+            model,dc,K_chols,RKHS_projs,x_test,y_test,
+            B=n_sample,M=8,device=device,
+            RKHS_PROJECTION_EACH_X_T=RKHS_PROJECTION_EACH_X_T,
+            PLOT_ANCESTRAL_STEPS=False,return_output=True)    
+ 
+    return train_losses, valid_losses, outs
